ee070d088c46f85fbd060d51c098a3079a060dab
Chris Capellini <chris@chriscap.com>
2026-02-18 20:58:37 -0500

    Initial Commit

---
+++ Aurae/AuraeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraeApp.swift
+//  Aurae
+//
+//  Created by Christopher Capellini on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
